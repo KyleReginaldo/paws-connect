15031d47682d9fbd946558d8e0541a9f0e42a99b
kyle reginaldo <97393306+KyleReginaldo@users.noreply.github.com>
2025-09-14 20:01:06 +0800

    Update route.ts

--- src/app/api/v1/send-email/route.ts
+++ src/app/api/v1/send-email/route.ts
@@ -13,11 +13,11 @@ export async function POST(request: Request) {
     const transporter = nodemailer.createTransport({
         // host: process.env.SMTP_HOST,
         service: 'gmail',
-        port: Number(process.env.SMTP_PORT) || 587,
+        port:  587,
         secure: false, // true for 465, false for other ports
         auth: {
             user: "pawsconnecttof@gmail.com",
-            pass: "etjg upbq huvf tusd",
+            pass: "nmeq gfvs igju jnio",
         },
         tls: {
             rejectUnauthorized: false,
@@ -27,11 +27,10 @@ export async function POST(request: Request) {
     try {
         // Send mail with defined transport object
         const info = await transporter.sendMail({
-            from: `"Paws Connect Support" <${process.env.EMAIL_FROM}>`, // sender address
-            to, // list of receivers
-            subject, // Subject line
-            html: text, // plain text body
-            // html: "<b>Hello world?</b>", // html body
+            from: `"Paws Connect Support" <pawsconnecttof@gmail.com>`, // sender address
+            to,
+            subject, 
+            html: text, 
         });
 
         console.log('Message sent: %s', info.messageId);
